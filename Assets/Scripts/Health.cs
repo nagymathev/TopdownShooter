75d47ea1d63a1c9db04af21254b02bb7658dfb06
Frictionpowered <denes@nagymathe.com>
2021-11-14 17:34:37 +0000

    Update Health.cs

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -7,28 +7,17 @@ public class Health : MonoBehaviour
     public float health;
     public int scoreReward = 100;
 
-    //private GameObject scoreBoard;
-    Score scorecomponent;
-
 	public GameObject prefabOnHurt;
 	public GameObject prefabOnDeath;
 
 	// Start is called before the first frame update
 	void Start()
     {
-         scorecomponent = GameObject.FindGameObjectWithTag("Score").GetComponent<Score>();
-        //score = score.GetComponent<Score>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (health <= 0)
-        {
-            scorecomponent.score += scoreReward;
-            OnKill();
-        }
-
     }
 
 	public void Hurt(float damage)
@@ -50,6 +39,10 @@ public class Health : MonoBehaviour
 
 	void OnKill()
     {
+		Score scorecomponent = GameObject.FindGameObjectWithTag("Score").GetComponent<Score>();
+		if (scorecomponent)
+			scorecomponent.score += scoreReward;
+
 		//ToDo: particles, audio, notify gameplay, etc
 		if (prefabOnDeath)
 			Instantiate(prefabOnDeath, transform.position, transform.rotation);
