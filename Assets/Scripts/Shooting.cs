e1cfe956185d9610a5312023ef15d82ba4867eb6
Frictionpowered <denes@nagymathe.com>
2021-11-14 17:23:27 +0000

    variable weapons

--- Assets/Scripts/Shooting.cs
+++ Assets/Scripts/Shooting.cs
@@ -4,25 +4,145 @@ using UnityEngine;
 
 public class Shooting : MonoBehaviour
 {
-
     public Transform firePoint;
-    public GameObject bulletPrefab;
+    //public GameObject bulletPrefab;
+	//public GameObject muzzleFlashPrefab;
+	//public GameObject reloadPrefab;
 
-    public float bulletForce = 20f;
+	[System.Serializable]
+	public struct WeaponDef
+	{
+		public GameObject bulletPrefab;
+		public GameObject muzzleFlashPrefab;
+		public GameObject reloadPrefab;
 
-    // Update is called once per frame
-    void Update()
+		public float bulletSpeed;// = 20f;
+
+		public float spreadAngle;// = 0.0f;
+		public int numBulletsFiredAtOnce;// = 1;
+
+		public bool autoRepeat;
+
+		public float repeatTime;// = 0.05f;
+
+		public float reloadTime;// = 1.0f;
+
+		public int magSize;// = 7;
+		public int numMagazines;// = -1;
+	}
+	public WeaponDef defaultWeapon;
+	public WeaponDef currentWeapon;
+
+	//public float bulletSpeed = 20f;
+
+	//public float spreadAngle = 0.0f;
+	//public int numBulletsFiredAtOnce = 1;
+
+	//public bool autoRepeat;
+
+	//public float repeatTime = 0.05f;
+	public float repeatTimer;
+
+	//public float reloadTime = 1.0f;
+	public float reloadTimer;
+
+	//public int magSize = 7;
+	public int numBullets = 10;
+	public int numMagazines = -1;
+
+	private void Start()
+	{
+		SetWeapon(defaultWeapon);
+	}
+
+	public void SetWeapon(WeaponDef def)
+	{
+		currentWeapon = def;
+		numBullets = currentWeapon.magSize;
+	}
+
+	// Update is called once per frame
+	void Update()
     {
-        if (Input.GetButtonDown("Fire1"))
-        {
-            Shoot();
-        }
+		if (reloadTimer > 0)
+		{//reloading
+			reloadTimer -= Time.deltaTime;
+			return;
+		}
+
+		if (repeatTimer > 0)
+		{
+			repeatTimer -= Time.deltaTime;
+			return;
+		}
+
+		if (currentWeapon.autoRepeat)
+		{
+			if (Input.GetButton("Fire1"))
+			{
+				Shoot();
+			}
+		} else
+		{
+			if (Input.GetButtonDown("Fire1"))
+			{
+				Shoot();
+			}
+		}
     }
 
     void Shoot()
     {
-        GameObject bullet = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
-        Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
-        rb.AddForce(firePoint.up * bulletForce, ForceMode2D.Impulse);
-    }
+		//if (!currentWeapon) return;
+
+		if (numBullets == 0)
+		{//empty
+			if (numMagazines > 0)
+			{//start reload
+				numMagazines--;
+				numBullets = currentWeapon.magSize;
+				reloadTimer = currentWeapon.reloadTime;
+				if (currentWeapon.reloadPrefab)
+					Instantiate(currentWeapon.reloadPrefab, firePoint.position, firePoint.rotation);
+				return;
+			} else
+			if (numMagazines < 0)
+			{//infinite mags (default pistol)
+				reloadTimer = currentWeapon.reloadTime;
+				numBullets = currentWeapon.magSize;
+				if (currentWeapon.reloadPrefab)
+					Instantiate(currentWeapon.reloadPrefab, firePoint.position, firePoint.rotation);
+				return;
+			} else
+			{//reset to default pistol
+			 //ToDo: how exactly?
+				SetWeapon(defaultWeapon);
+			}
+		}
+
+		if (currentWeapon.bulletPrefab)
+		{
+			for (int i = 0; i < currentWeapon.numBulletsFiredAtOnce; i++)
+			{
+				GameObject bullet = Instantiate(currentWeapon.bulletPrefab, firePoint.position, firePoint.rotation);
+				Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
+				//rb.AddForce(firePoint.up * bulletForce, ForceMode2D.Impulse);
+				Vector3 dir = firePoint.up;
+				if (currentWeapon.spreadAngle > 0)
+					dir = Quaternion.AngleAxis(Random.Range(-currentWeapon.spreadAngle, currentWeapon.spreadAngle), firePoint.forward) * dir;
+				rb.velocity = dir  * currentWeapon.bulletSpeed;
+			}
+		}
+		if (currentWeapon.muzzleFlashPrefab)
+			Instantiate(currentWeapon.muzzleFlashPrefab, firePoint.position, firePoint.rotation);
+
+		repeatTimer = currentWeapon.repeatTime;
+
+		if (numBullets < 0)
+		{//infinite ammo
+		} else
+		{
+			numBullets--;
+		}
+	}
 }
